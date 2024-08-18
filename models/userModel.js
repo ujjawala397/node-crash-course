c3d8cdc718c0cc88f92c8d7a81f0393e77e61cb1
ujjawal <ujjawala397@gmail.com>
2024-08-18 06:58:55 -0400

    user model, user controllers are made, which will help to login, register, and access

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose= require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username : {
+        type:String,
+        required:[true,"Please add username"],
+    },
+    email :{
+        type:String,
+        required:[true,"Please add user email address"],
+        unique:[true,"Email already exists"],
+    },
+    password:{
+        type:String,
+        required:[true,"Please add password"]
+    },
+    },
+    {
+        timestamps:true,
+    }
+);
+
+module.exports=mongoose.model("User",userSchema);
