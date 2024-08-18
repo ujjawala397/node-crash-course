94df1460f6989c9e194a51e41b82fb06599c7ce4
ujjawal <ujjawala397@gmail.com>
2024-08-18 10:15:58 -0400

    user registeration is done in database, using model, controller (controller stores the function logic)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,41 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User =require("../models/userModel");
 
 //Register user
 const registerUser = asyncHandler(async(req,res)=>{
+    const {username,email,password} =req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields required");
+    }
+
+    const userAvailable =await User.findOne({email});
+
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already Exist");
+    }
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+    console.log("Hashed Passwor : ",hashedPassword);
+    
+    const user = await User.create({
+        username,
+        email,
+        password :hashedPassword,
+    })
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({_id:user.id,email: user.email});
+    }
+    else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res.json({message:"Register user"});
+
 })
 
 // Login User
